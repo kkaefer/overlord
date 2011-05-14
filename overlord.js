f12af703c02baed3001e9208e216c3f9ea1be0f0
Konstantin Käfer <github@kkaefer.com>
2011-05-14 00:35:22 -0400

    first parts of web interface

--- overlord.js
+++ overlord.js
@@ -1,24 +1,31 @@
 #!/usr/bin/env node
+
+// Allow requiring .json files.
+require.extensions['.json'] = function (module, filename) {
+    module.exports = JSON.parse(require('fs').readFileSync(filename, 'utf8'));
+};
+
 var path = require('path');
 var express = require('express');
-var tools = require('./lib/tools');
 
 // Load configuration
 var argv = require('optimist').argv;
-if (!argv.files) argv.files = './files';
-argv.files = path.join(__dirname, argv.files);
+argv.files = path.join(__dirname, argv.files || 'files');
 
 var app = exports = module.exports = express.createServer();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.use(express.static(__dirname + '/assets'));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 app.use(express.bodyParser());
 
 app.post('/api/webhook', require('./lib/runner'));
-app.get('/', require('./lib/status'));
 
+app.get('/', require('./lib/status')['/']);
+app.get('/status', require('./lib/status')['/status']);
+app.get('/status/*', require('./lib/status')['/status/*']);
 
 if (!module.parent) {
     app.listen(3000);
